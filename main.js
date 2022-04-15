8c97e5eb0dadc760c2f5b93a9b11272be3575181
Jon Hii <jonathan.hii@outlook.com>
2022-04-16 09:52:07 +1000

    Moved css styles of gridDivs to javascript

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 4096; i++) {
   const gridDivs = document.createElement("div");
   gridDivs.id = "gridDivs";
+  gridDivs.style.outline = "1px solid black";
+  gridDivs.style.height = "10px";
+  gridDivs.style.width = "10px";
   container.appendChild(gridDivs);
 
   gridDivs.addEventListener("mouseover", () => {
