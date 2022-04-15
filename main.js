ed1670e6101af464bd6a5e3b8df3085b7f691e75
Jon Hii <jonathan.hii@outlook.com>
2022-04-16 09:27:12 +1000

    Created hover effect on grid divs + Changed grid to 64 * 64

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 4096; i++) {
   const gridDivs = document.createElement("div");
   gridDivs.id = "gridDivs";
   container.appendChild(gridDivs);
+
+  gridDivs.addEventListener("mouseover", () => {
+    gridDivs.style.backgroundColor = "black";
+  });
 }
