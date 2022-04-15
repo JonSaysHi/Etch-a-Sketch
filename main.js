77013bc3a97e323dc6dd5349971aa7503e8a546d
Jon Hii <jonathan.hii@outlook.com>
2022-04-15 15:38:57 +1000

    changed 16 to 256

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   const gridDivs = document.createElement("div");
   gridDivs.id = "gridDivs";
   container.appendChild(gridDivs);
