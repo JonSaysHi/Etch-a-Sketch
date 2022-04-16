091a5db3c701234e388268660c06c20814a3ff83
Jon Hii <jonathan.hii@outlook.com>
2022-04-17 09:16:23 +1000

    Duplicated + changed createGrid function to apply random RGB values on mouseover

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ const resetButton = document.querySelector("#reset-button");
 
 let gridSize = 16;
 
+//-- Function creates grid, changing the color of squares from white to black--//
+
+// function createGrid(gridSize) {
+//   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+//   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+//   for (let i = 0; i < gridSize * gridSize; i++) {
+//     const divs = document.createElement("div");
+//     container.appendChild(divs);
+//     divs.classList.add("gridDivs");
+//     divs.addEventListener("mouseover", () => {
+//       divs.style.backgroundColor = "black";
+//     });
+//   }
+// }
+
+//-- Function creates grid, changing the color fo squares from white to arand RGB value --//
+
+// function createGrid(gridSize) {
+//   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+//   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+//   for (let i = 0; i < gridSize * gridSize; i++) {
+//     const divs = document.createElement("div");
+//     container.appendChild(divs);
+//     divs.classList.add("gridDivs");
+//     divs.addEventListener("mouseover", () => {
+//       let randomColor = "#" + Math.floor(Math.random() * 19777215).toString(16);
+//       divs.style.backgroundColor = randomColor;
+//     });
+//   }
+// }
+
 function createGrid(gridSize) {
   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
@@ -11,7 +42,8 @@ function createGrid(gridSize) {
     container.appendChild(divs);
     divs.classList.add("gridDivs");
     divs.addEventListener("mouseover", () => {
-      divs.style.backgroundColor = "black";
+      let randomColor = "#" + Math.floor(Math.random() * 19777215).toString(16);
+      divs.style.backgroundColor = randomColor;
     });
   }
 }
@@ -28,7 +60,7 @@ function clearGrid() {
 }
 
 function sizeOfGrid() {
-  let gridSize = prompt("Please enter a grid size between 1-100 (x * x)", "");
+  let gridSize = prompt("Please enter a grid size between 1-100.", "");
   if (gridSize < 1 || gridSize > 100) {
     alert("ERROR! Grid size must be between 1-100.");
     createGrid(16);
