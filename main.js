c46796c609e96dfef5e3613c08c9a38e9b5fa7cb
Jon Hii <jonathan.hii@outlook.com>
2022-04-16 12:53:53 +1000

    Transferred CSS styles to javascript

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const container = document.querySelector("#container");
 const resetButton = document.querySelector("#reset-button");
 
+let gridSize = 64;
+
 function createGrid() {
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const gridDivs = document.createElement("div");
-    gridDivs.id = "gridDivs";
     gridDivs.classList.add("gridDivs");
     container.appendChild(gridDivs);
-
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     gridDivs.addEventListener("mouseover", () => {
       gridDivs.style.backgroundColor = "black";
     });
