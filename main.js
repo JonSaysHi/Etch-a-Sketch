f7bc496e3ffc4915ca70721ab78a0be53fa7ac45
Jon Hii <jonathan.hii@outlook.com>
2022-04-17 11:39:05 +1000

    Removed buttons and functionality for changes in color and opacity

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 const container = document.querySelector("#container");
-const resetButton = document.querySelector("#reset-button");
-const blackButton = document.querySelector("#black-button");
-const pencilButton = document.querySelector("#pencil-button");
-const rainbowButton = document.querySelector("#rainbow-button");
+// const sizeButton = document.querySelector("#size-button");
+const ResetButton = document.querySelector("#reset-button");
+// const blackButton = document.querySelector("#black-button");
+// const pencilButton = document.querySelector("#pencil-button");
+// const rainbowButton = document.querySelector("#rainbow-button");
 
 let gridSize = 16;
 
 //-- Function creates grid, changing the color of squares from white to black--//
 
-// function createGrid(gridSize) {
-//   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-//   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-//   for (let i = 0; i < gridSize * gridSize; i++) {
-//     const divs = document.createElement("div");
-//     container.appendChild(divs);
-//     divs.classList.add("gridDivs");
-//     divs.addEventListener("mouseover", () => {
-//       divs.style.backgroundColor = "black";
-//     });
-//   }
-// }
+function createGrid(gridSize) {
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const divs = document.createElement("div");
+    container.appendChild(divs);
+    divs.classList.add("gridDivs");
+
+    //mouseover event listener
+    divs.addEventListener("mouseover", () => {
+      divs.style.backgroundColor = "black";
+    });
+  }
+}
 
 //-- Function creates grid, changing the color fo squares from white to arand RGB value --//
 
@@ -37,32 +40,30 @@ let gridSize = 16;
 //   }
 // }
 
-function createGrid(gridSize) {
-  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const divs = document.createElement("div");
-    container.appendChild(divs);
-    divs.classList.add("gridDivs");
-    divs.addEventListener("mouseover", () => {
-      divs.style.backgroundColor = "black";
-      divs.style.opacity = (parseFloat(divs.style.opacity) || 0) + 0.2;
-    });
-  }
-}
+//-- Function creates grid, changes the color to increasing opacity --//
+
+// function createGrid(gridSize) {
+//   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+//   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+//   for (let i = 0; i < gridSize * gridSize; i++) {
+//     const divs = document.createElement("div");
+//     container.appendChild(divs);
+//     divs.classList.add("gridDivs");
+//     divs.addEventListener("mouseover", () => {
+//       divs.style.backgroundColor = "black";
+//       divs.style.opacity = (parseFloat(divs.style.opacity) || 0) + 0.2;
+//     });
+//   }
+// }
 
 createGrid(gridSize);
 
-resetButton.addEventListener("click", () => {
-  clearGrid();
-  sizeOfGrid();
+ResetButton.addEventListener("click", () => {
+  clear();
+  resize();
 });
 
-function clearGrid() {
-  document.querySelectorAll(".gridDivs").forEach((e) => e.remove());
-}
-
-function sizeOfGrid() {
+function resize() {
   let gridSize = prompt("Please enter a grid size between 1-100.", "");
   if (gridSize < 1 || gridSize > 100) {
     alert("ERROR! Grid size must be between 1-100.");
@@ -71,3 +72,9 @@ function sizeOfGrid() {
     createGrid(gridSize);
   }
 }
+
+function clear() {
+  document.querySelectorAll(".gridDivs").forEach((e) => e.remove());
+  createGrid(gridSize);
+}
+blackButton.addEventListener("click", () => {});
